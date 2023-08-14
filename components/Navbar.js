10f3b85175e9d44b416c4e5fca998cedc2ab888b
ziaullah667773 <114484191+ziaullah667773@users.noreply.github.com>
2023-08-14 14:28:27 -0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
         <div className="flex items-center font-medium justify-around">
           <div className="z-50 p-5 md:w-auto w-full flex justify-between">
             <h1 className="md:cursor-pointer h-9 text-3xl font-sans "><span className="text-white">Zia</span><span className="text-cyan-600">Ullah</span></h1>
-            <div className="text-3xl md:hidden" onClick={() => setOpen(!open)}>
+            <div className="text-3xl md:hidden text-white" onClick={() => setOpen(!open)}>
               {open ? <AiOutlineClose /> : <FiMenu />}
             </div>
           </div>
