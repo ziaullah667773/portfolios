4d68e120b6d1c8bf7f5f5360e70df8fbfe65765c
ziaullah667773 <114484191+ziaullah667773@users.noreply.github.com>
2023-08-14 11:43:43 -0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -38,6 +38,19 @@ const Navbar = () => {
               About
               </Link>
             </li>
+            <li>
+              <Link href={"/portfolio"} className="py-7 px-3 inline-block">
+              Portfolio
+              </Link>
+            </li>  <li>
+              <Link href={"/about"} className="py-7 px-3 inline-block">
+              About
+              </Link>
+            </li>  <li>
+              <Link href={"/about"} className="py-7 px-3 inline-block">
+              About
+              </Link>
+            </li>
             <NavLinks />
             <li>
               <Link href={"/skills"} className="py-7 px-3 inline-block">
